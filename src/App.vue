c348099752a71b74ca44c81d6607085a08ce1817
Jon Herbert <jonherbertmusic@gmail.com>
2021-12-28 13:35:43 +0000

    brken but will fix

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/characterinventory">Character Inventory</router-link>
-      <ul
-        class="list-unstyled components"
-        :v-for="
-          (route, index) in
-          $router.options.routes.filter((x) => x.path == $route.matched[0].path)
-        "
-      >
-        <li :v-for="child in route.children">
-          <a class="nav-item" :key="index">
-            <router-link
-              :to="route.path + '/' + child.path"
-              exact-active-class="active"
-            >
-              <icon :icon="route.icon" class="mr-2" /><span>{{
-                child.name
-              }}</span>
-            </router-link>
-          </a>
-        </li>
-      </ul>
+      <router-link to="/characterinventory">Character Inventory</router-link> |
+      <router-link to="/characteroverview">Character Overview</router-link>
     </div>
     <router-view />
   </w-app>
--- src/views/Character/CharacterOverview.vue
+++ src/views/Character/CharacterOverview.vue
@@ -1,10 +1,34 @@
 <template>
-  <div></div>
+  <div>
+    <ul
+      class="list-unstyled components"
+      :v-for="
+        (route, index) in
+        $router.options.routes.filter((x) => x.path == $route.matched[0].path)
+      "
+    >
+      <li :v-for="child in route.children">
+        <a class="nav-item" :key="index">
+          <router-link
+            :to="route.path + '/' + child.path"
+            exact-active-class="active"
+          >
+            <icon :icon="route.icon" class="mr-2" /><span>{{
+              child.name
+            }}</span>
+          </router-link>
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+
 export default {
-  setup () { }
+  setup () {
+    console.log($router.options.route)
+  }
 }
 </script>
 
